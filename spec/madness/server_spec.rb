60cdddfdd31eed2445c3e54bd0b5b3594b2e09d8
DannyB <dannyb@telesofia.com>
2016-06-10 19:16:09 +0000

    initial server spec

--- spec/madness/server_spec.rb
+++ spec/madness/server_spec.rb
@@ -7,4 +7,12 @@ describe Server do
     expect(last_response).to be_ok
   end
 
+  context "in subfolders" do
+    it "shows breadcrumbs" do
+      get '/Folder'
+      expect(last_response).to be_ok
+      expect(last_response.body).to include '<div class="breadcrumbs">'
+    end
+  end
+
 end
