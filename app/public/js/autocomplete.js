094058b55fe75e78ec415d9ed8ce8737c95bd1bd
Danny Ben Shitrit <db@dannyben.com>
2018-08-10 15:05:09 +0000

    update autocomplete style and options

--- app/public/js/autocomplete.js
+++ app/public/js/autocomplete.js
@@ -2,7 +2,14 @@ $(function() {
   $('.autocomplete').autocomplete({
     serviceUrl: '/_search/autocomplete',
     minChars: 2,
+    noCache: false,
+    preventBadQueries: false,
     triggerSelectOnValidInput: false,
-    autoSelectFirst: true,
+    autoSelectFirst: false,
+    preserveInput: false,
+    showNoSuggestionNotice: true,
+    onSelect: function(suggestion) {
+      window.location = "/" + suggestion.value;
+    }
   });
 })
--- lib/madness/server.rb
+++ lib/madness/server.rb
@@ -19,7 +19,8 @@ module Madness
       return { suggestions: [] } if query.empty?
 
       results = Search.new.search query
-      suggestions = results.map { |r| r[:label] }
+      suggestions = results.map { |r| r[:label] } # or r[:file]
+      
       response = { suggestions: suggestions }
       response.to_json
     end
