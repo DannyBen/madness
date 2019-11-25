116844313cb14c5f9fc3927d7fbf91b7d42d707c
Danny Ben Shitrit <db@dannyben.com>
2019-11-25 18:04:26 +0000

    initial copy to clipboard implementation

---
+++ app/public/js/clipboard.js
@@ -0,0 +1,11 @@
+$(function() {
+  new ClipboardJS('.clipboard-button', {
+    target: function(trigger) {
+      return trigger.parentElement;
+    }
+  });
+
+  html = '<a href="#" class="clipboard-button" data-clipboard-target=".code"><i class="icon-export"></i></a>';
+
+  $('.CodeRay pre').prepend(html);
+})
