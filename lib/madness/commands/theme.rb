44ee5a53cc6d001550b8419bf516c58870e3ba14
Danny Ben Shitrit <db@dannyben.com>
2023-01-19 14:39:20 +0000

    - Add 'madness theme css' command to copy the public css

--- lib/madness/commands/theme.rb
+++ lib/madness/commands/theme.rb
@@ -4,20 +4,31 @@ module Madness
       summary 'Create files for theme customization'
 
       usage 'madness theme full [PATH]'
+      usage 'madness theme css'
       usage 'madness config (-h|--help)'
 
       command 'full', 'Create a full theme customization directory in PATH [default: _theme]'
+      command 'css', 'Create a folder with the CSS file for customization'
 
       def full_command
-        raise InitError, "Directory #{path} already exists" if Dir.exist? path
+        raise InitError, "Directory #{theme_path} already exists" if Dir.exist? theme_path
 
-        FileUtils.cp_r File.expand_path('../../../app', __dir__), path
-        say "!txtgrn!Created #{path} theme folder"
+        FileUtils.cp_r File.expand_path('../../../app', __dir__), theme_path
+        say "!txtgrn!Created #{theme_path} theme folder"
+      end
+
+      def css_command
+        file = 'css/main.css'
+        raise InitError, "File #{file} already exists" if File.exist? file
+
+        FileUtils.mkdir_p 'css'
+        FileUtils.cp_r File.expand_path('../../../app/public/css/main.css', __dir__), 'css/main.css'
+        say '!txtgrn!Created css/main.css'
       end
 
     private
 
-      def path
+      def theme_path
         args['PATH'] || '_theme'
       end
     end
