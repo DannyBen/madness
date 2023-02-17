38af6e4d415dcd4a29bff782739b6c1f0b0e9723
Danny Ben Shitrit <db@dannyben.com>
2023-02-17 09:39:37 +0000

    - Add support for CSS partials and 'madness theme colors' command

--- lib/madness/commands/theme.rb
+++ lib/madness/commands/theme.rb
@@ -5,10 +5,12 @@ module Madness
 
       usage 'madness theme full [PATH]'
       usage 'madness theme css'
+      usage 'madness theme colors'
       usage 'madness config (-h|--help)'
 
       command 'full', 'Create a full theme customization directory in PATH [default: _theme]'
-      command 'css', 'Create a folder with the CSS file for customization'
+      command 'css', 'Create a folder with the entire CSS file for customization'
+      command 'colors', 'Create a folder with CSS for overriding colors only'
 
       def full_command
         raise InitError, "Directory #{theme_path} already exists" if Dir.exist? theme_path
@@ -18,15 +20,22 @@ module Madness
       end
 
       def css_command
-        file = 'css/main.css'
-        raise InitError, "File #{file} already exists" if File.exist? file
+        copy_file 'app/public/css/main.css', 'css/main.css'
+      end
 
-        FileUtils.mkdir_p 'css'
-        FileUtils.cp_r File.expand_path('../../../app/public/css/main.css', __dir__), 'css/main.css'
-        say 'Created g`css/main.css`'
+      def colors_command
+        copy_file 'app/styles/_variables.scss', 'css/colors.css'
       end
 
     private
+
+      def copy_file(source, target)
+        raise InitError, "File #{target} already exists" if File.exist? target
+
+        FileUtils.mkdir_p File.dirname(target)
+        FileUtils.cp_r File.expand_path("../../../#{source}", __dir__), target
+        say "Created g`#{target}`"
+      end
 
       def theme_path
         args['PATH'] || '_theme'
