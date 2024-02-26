0fd9f8571b0b2ceeb1b93464dfdf1c71e320f70f
Danny Ben Shitrit <db@dannyben.com>
2024-02-26 08:44:03 +0000

    fix typos

--- lib/madness/server_base.rb
+++ lib/madness/server_base.rb
@@ -16,7 +16,7 @@ module Madness
     set :static, false
 
     # Since we cannot use any config values in the main body of the class,
-    # since they will be updated later, we need to set anything that relys
+    # since they will be updated later, we need to set anything that relies
     # on the config values just before running the server.
     # The CommandLine class and the test suite should both call
     # `Server.prepare` before calling Server.run!
