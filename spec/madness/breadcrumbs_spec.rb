509b23ba1c229714df9b9eef2ac8264a2855595c
Danny Ben Shitrit <db@dannyben.com>
2024-04-10 04:28:23 +0000

    - Refactor breadcrumbs data structure

--- spec/madness/breadcrumbs_spec.rb
+++ spec/madness/breadcrumbs_spec.rb
@@ -4,10 +4,10 @@ describe Breadcrumbs do
   describe '#links' do
     let(:links) { subject.links }
 
-    it 'returns an array of OpenStructs' do
+    it 'returns an array of Breadcrumb structs' do
       expect(links).to be_an Array
       expect(links.count).to eq 4
-      expect(links.first).to be_an OpenStruct
+      expect(links.first).to be_a described_class::Breadcrumb
     end
 
     it 'adds a home link' do
@@ -15,17 +15,13 @@ describe Breadcrumbs do
       expect(links.first.href).to eq '/'
     end
 
-    it 'adds a link to each element' do
+    it 'adds a link to all elements except the last one' do
       expect(links[1].label).to eq 'one'
       expect(links[1].href).to eq '/one/'
       expect(links[2].label).to eq 'two'
       expect(links[2].href).to eq '/one/two/'
       expect(links[3].label).to eq 'three'
-      expect(links[3].href).to eq '/one/two/three/'
-    end
-
-    it 'adds a last attribute to last element' do
-      expect(links.last.last).to be true
+      expect(links[3].href).to be_nil
     end
 
     context 'with sorted elements' do
@@ -37,7 +33,7 @@ describe Breadcrumbs do
         expect(links[2].label).to eq 'two'
         expect(links[2].href).to eq '/1. one/2. two/'
         expect(links[3].label).to eq 'three'
-        expect(links[3].href).to eq '/1. one/2. two/3. three/'
+        expect(links[3].href).to be_nil
       end
     end
 
@@ -48,7 +44,7 @@ describe Breadcrumbs do
       it 'prepends the links with the base_uri' do
         expect(links[1].href).to eq '/docs/one/'
         expect(links[2].href).to eq '/docs/one/two/'
-        expect(links[3].href).to eq '/docs/one/two/three/'
+        expect(links[3].href).to be_nil
       end
     end
   end
